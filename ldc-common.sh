cac633204499d75a2a471fd80d55c8185951d22e
ewsdocker <ewsdocker@gmail.com>
2020-12-28 22:30:56 -0700

    created ldc-common library to use with ldc builds

---
+++ ldc-common.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+declare ldcextv="-b4"
+
+declare ldcbasev="0.1.0"
+declare ldcvers="-${ldcbasev}"
+
+declare libver="0.1.6"
+declare ldclib="-${libver}"
+
+declare ldccont="${ldcvers}"
+
+declare pkgnet="pkgnet"
+declare webnet="webnet"
+
+#declare pkgserver="http://alpine-nginx-pkgcache"
+declare pkgserver="http://anginx-pkgcache"
+

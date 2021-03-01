04b0b65c9427134e24d04a59afd9ee7b312d1ae3
ewsdocker <ewsdocker@gmail.com>
2021-02-28 20:48:34 -0700

    update to -b5

--- ldc-common.sh
+++ ldc-common.sh
@@ -1,18 +1,35 @@
 #!/bin/bash
 
-declare ldcextv="-b4"
+# ==========================================
+# ==========================================
+
+declare ldcalpine="3.12"
+declare ldcdebian="10.7"
+
+# ==========================================
 
 declare ldcbasev="0.1.0"
 declare ldcvers="-${ldcbasev}"
+declare ldcextv="-b4"
+
+declare ldccont="${ldcvers}"
+
+# ==========================================
 
 declare libver="0.1.6"
 declare ldclib="-${libver}"
 
-declare ldccont="${ldcvers}"
+# ==========================================
 
 declare pkgnet="pkgnet"
 declare webnet="webnet"
 
-#declare pkgserver="http://alpine-nginx-pkgcache"
+declare nginxnet="nginx-net"
+
+# ==========================================
+
 declare pkgserver="http://anginx-pkgcache"
+
+# ==========================================
+# ==========================================
 
